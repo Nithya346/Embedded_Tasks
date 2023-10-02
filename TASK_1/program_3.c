ab1b8789723beee0260915f575225f31e8b334ce
Nithya346 <130446075+Nithya346@users.noreply.github.com>
2023-10-02 18:33:19 +0530

    Update program_3.c

--- TASK_1/program_3.c
+++ TASK_1/program_3.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    int n,j,i,sum=0;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+         for(j=1;j<=n;j++)
+         {
+             sum=i+j;
+             printf("%d\t",sum);
+         }
+        printf("\n");
+    }
+}
