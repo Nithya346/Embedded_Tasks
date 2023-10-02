0612c06d80ee90fbf04ba239095064f4304e411d
Nithya346 <130446075+Nithya346@users.noreply.github.com>
2023-10-02 18:28:21 +0530

    Update program_2.c

--- TASK_1/program_2.c
+++ TASK_1/program_2.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int n,j,i;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+         for(j=1;j<=n;j++)
+         {
+             printf("1");
+         }
+        printf("\n");
+    }
+}
